8f76ef38d5b4b90a3cf5decc5b52373bc32e6521
SMaerker <maerkerseth3@gmail.com>
2017-06-18 20:16:09 -0700

    Beginnings of hill generation
    It's so disgusting oh my god I'm so sorry. Soon I will clean it up. I didn't have too much time today to work on it.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -15,13 +15,10 @@ int _tmain(int argc, _TCHAR* argv[]){
 
 	srand(time(0));
 
-
-	
 	sf::Font testfont;
-	
-
 	testfont.loadFromFile("ariblk.ttf");
 
+	const int radiusMin = 3, radiusMax = 10;
 	
 
 	int width = 800, height = 600;
@@ -31,18 +28,55 @@ int _tmain(int argc, _TCHAR* argv[]){
 	sf::Clock clock;
 
 	Tile **Grid = new Tile*[800 / 10];
-
 	for (int r = 0; r < 80; r++){
 		Grid[r] = new Tile[60];
 	}
 
+	////////////////////////////
+	int d = rand() % 60 + 1;
+
+	int *pointsX = new int[d], *pointsY = new int[d];
+
+	for (int c = 0; c < d; c++){
+		pointsX[c] = rand() % 80;
+		pointsY[c] = rand() % 60;
+	//	printf("%i - %i\n", pointsX[c], pointsY[c]);
+	}
+	////////////////////////////
+	////////////////////////////
 	for (int r = 0; r < 80; r++){
 		for (int c = 0; c < 60; c++){
-			Grid[r][c].setOffset(sf::Vector2f(static_cast<float>(r), static_cast<float>(c)), testfont);
-			Grid[r][c].setVal(rand() % 9 + 1);
-			printf("%i ", Grid[r][c].getVal());
+			//this is about to get super gross, sorry to any future person reading this. Hopefully whatever god there is 
+			//can forgive me for this sin.
+			for (int x = 0; x < d; x++){
+				if (r == pointsX[x] && c == pointsY[x]){
+					
+					//Random point found, generate hill.
+					int r = rand() % radiusMax + radiusMin;
+					float z = pow(static_cast<float>(r), 2);
+				
+					
+
+					Grid[r][c].setVal(static_cast<int>(z));
+
+					
+
+				}
+			}
+
+
 		}
-		printf("\n");
+	}
+	////////////////////////////
+	for (int r = 0; r < 80; r++){
+		for (int c = 0; c < 60; c++){
+			//Grid[r][c].setVal(0);
+
+			Grid[r][c].setOffset(sf::Vector2f(static_cast<float>(r), static_cast<float>(c)), testfont);
+			
+			//printf("%i ", Grid[r][c].getVal());
+		}
+	//	printf("\n");
 	}
 
 	while (window.isOpen()){
